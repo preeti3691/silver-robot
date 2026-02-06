cb9d16eb4eb6199cabfcfe32c95361af421d189f
preeti3691 <preetij3691@gmail.com>
2026-02-06 23:35:58 +0530

    Initial commit with assignments

---
+++ Assignment_1/Task_1.py
@@ -0,0 +1,11 @@
+#  Takes two numbers as input from the user.
+
+first_Number = int(input("Enter first number: "))
+second_Number = int(input("Enter second number: "))
+
+#Performs the basic mathematical operations on these two numbers:
+print()
+print(f"Addition : {first_Number + second_Number}")
+print(f"Subtraction : {first_Number - second_Number}")
+print(f"Multiplication : {first_Number * second_Number}")
+print(f"Division : {first_Number / second_Number}")
---
+++ Assignment_1/Task_2.py
@@ -0,0 +1,8 @@
+# Takes a user's first name and last name as input.
+
+First_name = input("Enter your first name: ")
+Last_name = input("Enter your last name: ")
+
+# Prints a personalized greeting message using the full name.
+
+print(f"Hello,{First_name} {Last_name}! Welcome to the Python program.")
