8e5392694ed222289d170ea8696cda6384b6b7cc
preeti3691 <preetij3691@gmail.com>
2026-02-07 17:02:45 +0530

    Initial commit with python assignments

---
+++ Assignment_2/Task1_control_structure_in_python.py
@@ -0,0 +1,9 @@
+# Check if a Number is Even or Odd
+num = int(input("Enter a number: "))
+
+#Checks whether the number is even or odd using an if-else statement.
+
+if num % 2 == 0:
+    print(f" {num} is an even number.")
+else:
+    print(f" {num} is an odd number.")
